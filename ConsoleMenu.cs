30b580040253c7e08db359e454325ab7f39e7343
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2021-02-19 18:31:19 +0200

    Yes / No - first attempt, possible rework

--- ConsoleMenu.cs
+++ ConsoleMenu.cs
@@ -8,6 +8,8 @@ namespace phil_console_ui_ibrary
     /// </summary>
     public class Menu
     {
+        private enum YesNoOption { Yes, No };
+
         /// <summary>
         /// Displays a menu and returns a value representing the index of the option selected
         /// </summary>
@@ -73,25 +75,98 @@ namespace phil_console_ui_ibrary
                     default:
                         Console.Beep();
                         break;
-
                 }
-
-
             } while (selectedOption == -1);
             Console.CursorVisible = true;
             return selectedOption;
         }
-        private static void WriteClearedActiveOption(string option, int cursorYPos, int maxOptionLength = 0)
+
+        /// <summary>
+        /// Displays a Yes / No option
+        /// </summary>
+        /// <param name="activeYesColor"></param>
+        /// <param name="activeNoColor"></param>
+        /// <param name="activeBgColor">The background color for the active option</param>
+        /// <returns></returns>
+        public static Boolean YesNo(ConsoleColor activeYesColor = ConsoleColor.Green, ConsoleColor activeNoColor = ConsoleColor.Red, ConsoleColor activeBgColor = ConsoleColor.Black)
+        {
+            int currentYPos = Console.GetCursorPosition().Top;
+            var currentOption = YesNoOption.Yes;
+            string selectedOption = null;
+            Console.CursorVisible = false;
+            WriteYesNoOption(currentOption, currentYPos, activeYesColor, activeNoColor, activeBgColor);
+            do
+            {
+                var ck = Console.ReadKey();
+                switch (ck.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        if (currentOption==YesNoOption.No)
+                        {
+                            currentOption = YesNoOption.Yes;
+                        }
+                        WriteYesNoOption(currentOption, currentYPos, activeYesColor, activeNoColor, activeBgColor);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (currentOption == YesNoOption.Yes)
+                        {
+                            currentOption = YesNoOption.No;
+                        }
+                        WriteYesNoOption(currentOption, currentYPos, activeYesColor, activeNoColor, activeBgColor);
+                        break;
+                    case ConsoleKey.Y:
+                        currentOption = YesNoOption.Yes;
+                        WriteYesNoOption(currentOption, currentYPos, activeYesColor, activeNoColor, activeBgColor);
+                        selectedOption = YesNoOption.Yes.ToString();
+                        break;
+                    case ConsoleKey.N:
+                        currentOption = YesNoOption.No;
+                        WriteYesNoOption(currentOption, currentYPos, activeYesColor, activeNoColor, activeBgColor);
+                        selectedOption = YesNoOption.No.ToString();
+                        break;
+                    case ConsoleKey.Enter:
+                        selectedOption = currentOption.ToString();
+                        break;
+                    default:
+                        Console.Beep();
+                        break;
+                }
+
+            } while (selectedOption == null);
+
+
+            Console.CursorVisible = true;
+            return selectedOption == YesNoOption.Yes.ToString();
+
+        }
+        private static void WriteYesNoOption(YesNoOption activeOption, int cursorYPos, ConsoleColor activeYesColor, ConsoleColor activeNoColor, ConsoleColor activeBgColor)
         {
             Console.SetCursorPosition(0, cursorYPos); Console.ResetColor();
+            if (activeOption==YesNoOption.Yes)
+            {
+                Console.ForegroundColor = activeYesColor; Console.BackgroundColor = activeBgColor;
+            }
+            Console.Write("[Yes]"); Console.ResetColor(); Console.Write(" ");
+            if (activeOption==YesNoOption.No)
+            {
+                Console.ForegroundColor = activeNoColor; Console.BackgroundColor = activeBgColor;
+            }
+            Console.Write("[No]"); Console.ResetColor();
+        }
+
+        private static void WriteClearedActiveOption(string option, int cursorYPos, int maxOptionLength = 0, int cursorXPos = 0)
+        {
+            Console.SetCursorPosition(cursorXPos, cursorYPos); Console.ResetColor();
             Console.Write(option.PadRight(maxOptionLength));
         }
-        private static void WriteActiveOption(string option, int cursorYPos, ConsoleColor activeColor, ConsoleColor activeBgColor, int maxOptionLength = 0)
+        private static void WriteActiveOption(string option, int cursorYPos, ConsoleColor activeColor, ConsoleColor activeBgColor, int maxOptionLength = 0, int cursorXPos = 0)
         {
-            Console.SetCursorPosition(0, cursorYPos);
+            Console.SetCursorPosition(cursorXPos, cursorYPos);
             Console.ForegroundColor = activeColor; Console.BackgroundColor = activeBgColor;
             Console.Write(option.PadRight(maxOptionLength));
         }
     }
+
+
 
 }
